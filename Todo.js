089b8c975c3356693c8a93050ae92f70162f981b
Scott Adams <ScottAdams@ScottAdams.me>
2018-10-19 15:00:08 -0400

    added basic styling for different estimate todos

--- Todo.js
+++ Todo.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { TouchableHighlight, View, Text, Button } from 'react-native';
 import Swipeout from 'react-native-swipeout';
+
+colorForEstimate = {
+    5:'#CADBF7',
+    30:'#D9EAD4',
+    60:'#FBE4CE',
+    240:'#E4B8AF'
+}
 export default class Todo extends React.PureComponent {
     // toggle a todo as completed or not via update()
     toggleComplete() {
@@ -8,6 +15,8 @@ export default class Todo extends React.PureComponent {
             complete: !this.props.complete,
         });
     }
+
+
 
     render() {
         const swipeoutBtns = [
@@ -21,7 +30,7 @@ export default class Todo extends React.PureComponent {
             }
         ]
         return (
-          <View style={{margin: 5, padding:0, borderTopWidth: 1, borderBottomWidth: 1, borderColor: '#dddddd'}}>
+          <View style={{paddingTop: this.props.estimate/4, paddingBottom: this.props.estimate/4, margin: 2, padding:5, borderRadius:4, backgroundColor: colorForEstimate[this.props.estimate]}}>
               <View style={{ flex: 1, height: 48, flexDirection: 'row', alignItems: 'center' }}>
                   <View style={{ flex: 8 }}>
                       <Text>{this.props.title}</Text>
