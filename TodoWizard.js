d4fccf36037cf2ca7c4d158722e6d86198347399
Scott Adams <ScottAdams@ScottAdams.me>
2018-10-19 15:17:17 -0400

    added InputAccessory so wizard stays above keyboard on iOS

--- TodoWizard.js
+++ TodoWizard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TextInput, InputAccessoryView, TouchableHighlight } from 'react-native';
+import { View, Text, TextInput, InputAccessoryView, TouchableHighlight, StyleSheet, Button } from 'react-native';
 export default class TodoWizard extends React.PureComponent {
     // toggle a todo as completed or not via update()
     constructor() {
@@ -24,13 +24,13 @@ export default class TodoWizard extends React.PureComponent {
       let wizardView = <View/>;
         if(!this.state.title) {
           wizardView = (
-            <View>
+            <View style={styles.wizardViewContainer}>
               <TextInput value={this.state.titleInput} onChangeText={(text) => this.updateTitle(text)} placeholder="New Todo"/>
-              <TouchableHighlight onPress={() => this.setTitle()}><Text>Create</Text></TouchableHighlight>
+              <TouchableHighlight onPress={() => this.setTitle()}><Text>Next</Text></TouchableHighlight>
             </View>);
         } else if(!this.state.estimate) {
           wizardView = (
-            <View>
+            <View style={styles.wizardViewContainer}>
               <TouchableHighlight onPress={() => this.setEstimate(5)}><Text>5 Min</Text></TouchableHighlight>
               <TouchableHighlight onPress={() => this.setEstimate(30)}><Text>30 Min</Text></TouchableHighlight>
               <TouchableHighlight onPress={() => this.setEstimate(60)}><Text>1 hr</Text></TouchableHighlight>
@@ -38,12 +38,21 @@ export default class TodoWizard extends React.PureComponent {
             </View>);
         } else if(!this.state.priority) {
         wizardView = (
-          <View>
+          <View style={styles.wizardViewContainer}>
             <TouchableHighlight onPress={() => this.setPriority('today')}><Text>Today</Text></TouchableHighlight>
             <TouchableHighlight onPress={() => this.setPriority('week')}><Text>This Week</Text></TouchableHighlight>
             <TouchableHighlight onPress={() => this.setPriority('month')}><Text>This Month</Text></TouchableHighlight>
           </View>);
         }
-        return wizardView
+        return (<InputAccessoryView>{wizardView}</InputAccessoryView>)
     }
 }
+
+const styles = StyleSheet.create({
+  wizardViewContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: '#eeeeee',
+    padding: 10
+  }
+})
